1fac1e0c1a8f31e4ff33bca0f3b03f753f0d0a34
Leonardo Arabe Castedo <a2020122243@estudiantes.upsa.edu.bo>
2021-03-27 11:07:16 -0400

    Modelo, DataContext y controlador incorporado

---
+++ Sol_Recuperacion_LeonardoArabe/Recuperacion_LeonardoArabe/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Recuperacion_LeonardoArabe.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Sol_Recuperacion_LeonardoArabe/Recuperacion_LeonardoArabe/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Recuperacion_LeonardoArabe.Models
+{
+    public class Product
+    {
+        [Key]
+        [Range(1,99999)]
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength =3)]
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime LastBuy { get; set; }
+    }
+}
