212ae04be3b2ce1fbdbdcb268f9251c28c8f0abf
Leonardo Arabe Castedo <a2020122243@estudiantes.upsa.edu.bo>
2021-03-27 11:41:13 -0400

    Se incorporó Post al Add Controller

--- Sol_Recuperacion_LeonardoArabe/Recuperacion_LeonardoArabe/Controllers/AddController.cs
+++ Sol_Recuperacion_LeonardoArabe/Recuperacion_LeonardoArabe/Controllers/AddController.cs
@@ -10,7 +10,14 @@ namespace Recuperacion_LeonardoArabe.Controllers
     public class AddController : ApiController
     {
         [HttpGet]
+        [HttpPost]
         public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+        [HttpPost]
+        [Route("{a}/{b}")]
+        public int Sumar2(int a, int b)
         {
             return a + b;
         }
