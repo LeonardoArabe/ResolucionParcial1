a6fc403cd6b065092445bdee3f8ad062dfe2066a
Leonardo Arabe Castedo <a2020122243@estudiantes.upsa.edu.bo>
2021-03-27 11:28:52 -0400

    Se incorporó Add controller con Get

---
+++ Sol_Recuperacion_LeonardoArabe/Recuperacion_LeonardoArabe/Controllers/AddController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Recuperacion_LeonardoArabe.Controllers
+{
+    public class AddController : ApiController
+    {
+        [HttpGet]
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
